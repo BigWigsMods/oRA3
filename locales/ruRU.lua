7b65914d432ea805375c501485a4ac298f98f8ba
StingerSoft <StingerSoft@users.noreply.github.com>
2009-04-07 20:20:21 +0000

    added ruRU

---
+++ locales/ruRU.lua
@@ -0,0 +1,27 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("oRA3", "ruRU")
+
+-- Generic
+L["Name"] = "Имя"
+L["Config"] = "Настройки"
+
+-- Ready check module
+L["The following players are not ready: %s"] = "Следующие игроки не готовы: %s"
+L["Ready check over in %d seconds"] = "Проверка готовности через %d секунд"
+L["Ready"] = "Готов"
+L["Not Ready"] = "Не готов"
+L["No Response"] = "Нет ответа"
+L["Offline"] = "Не в сети"
+
+-- Durability module
+L["Durability"] = "Прочность"
+L["Average"] = "Среднее"
+L["Broken"] = "Сломан"
+L["Minimum"] = "Минимум"
+
+-- Resistances module
+L["Resistances"] = "Сопротивление"
+L["Frost"] = "Льду"
+L["Fire"] = "Огню"
+L["Shadow"] = "Тёмной магии"
+L["Nature"] = "Магии природы"
+L["Arcane"] = "Тайной магии"
