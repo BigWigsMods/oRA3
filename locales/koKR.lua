9152f8b0ebbe54cbd1646b092aaa043c5952d258
7destiny <7destiny@users.noreply.wowace.com>
2009-04-07 20:38:27 +0000

    Add koKR Locale

---
+++ locales/koKR.lua
@@ -0,0 +1,27 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("oRA3", "koKR")
+
+-- Generic
+L["Name"] = "이름"
+L["Config"] = "설정"
+
+-- Ready check module
+L["The following players are not ready: %s"] = "준비가 되지 않은 플레이어: %s"
+L["Ready check over in %d seconds"] = "%d 초 이내에 준비 확인 종료"
+L["Ready"] = "준비 완료"
+L["Not Ready"] = "준비 안됨"
+L["No Response"] = "응답 없음"
+L["Offline"] = "오프라인"
+
+-- Durability module
+L["Durability"] = "내구도"
+L["Average"] = "평균"
+L["Broken"] = "파손"
+L["Minimum"] = "분"
+
+-- Resistances module
+L["Resistances"] = "저항력"
+L["Frost"] = "냉기"
+L["Fire"] = "화염"
+L["Shadow"] = "암흑"
+L["Nature"] = "자연"
+L["Arcane"] = "비전"
