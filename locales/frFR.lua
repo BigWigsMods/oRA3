8226e200f58cae0c91bd4d7f5bc099c85a4d080b
tborlee <tborlee@users.noreply.github.com>
2009-06-10 11:28:12 +0000

    frFR Update

--- locales/frFR.lua
+++ locales/frFR.lua
@@ -1,7 +1,5 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("oRA3", "frFR")
-
 if not L then return end
-
 -- Generic
 L["Name"] = "Nom"
 L["Checks"] = "Vérif."
@@ -22,6 +20,9 @@ L["SHAMAN"] = "Chaman"
 L["ROGUE"] = "Voleur"
 L["DEATHKNIGHT"] = "Chevalier de la mort"
 
+-- Core
+L["You can configure some options here. All the actual actions are done from the panel at the RaidFrame."] = "Vous pouvez configurer quelques options ici. Tout le reste se trouve dans le panneau de l'onglet raid."
+
 -- Ready check module
 L["The following players are not ready: %s"] = "Les joueurs suivants ne sont pas prêts : %s"
 L["Ready check (%d seconds)"] = "Appel (%d |4seconde:secondes;)."
@@ -29,6 +30,11 @@ L["Ready"] = "Prêt"
 L["Not Ready"] = "Pas prêt"
 L["No Response"] = "Pas de réponse"
 L["Offline"] = "Hors ligne"
+L["Play a sound when a ready check is performed."] = "Joue un son quand un appel est lancé."
+L["GUI"] = "GUI"
+L["Show the oRA3 Ready Check GUI when a ready check is performed."] = "Affiche le GUI d'appel de oRA3 quand un appel est lancé."
+L["Auto Hide"] = "Masquer auto."
+L["Automatically hide the oRA3 Ready Check GUI when a ready check is finished."] = "Masque automatiquement le GUI d'appel de oRA3 quand un appel est terminé."
 
 -- Durability module
 L["Durability"] = "Durabilité"
@@ -75,7 +81,7 @@ L["Guild"] = "Guilde"
 L["Promote all guild members automatically."] = "Nomme automatiquement assistants tous les membres de votre guilde."
 L["By guild rank"] = "Par rang de guilde"
 L["Individual promotions"] = "Promotions individuelles"
-L["Note that names are case sensitive. To add a player, enter a player name in the box below and hit Enter or click the button that pops up. To remove a player from being promoted automatically, just click his name in the dropdown below."] = "Notez que les noms sont sensibles à la casse. Pour ajouter un joueur, entrez son nom dans la boîte de saisie ci-dessous et appuyez sur Entrée o u cliquez sur le bouton qui apparaît. Pour enlever un joueur, cliquez tout simplement sur son nom dans le menu déroulant ci-dessous."
+L["Note that names are case sensitive. To add a player, enter a player name in the box below and hit Enter or click the button that pops up. To remove a player from being promoted automatically, just click his name in the dropdown below."] = "Notez que les noms sont sensibles à la casse. Pour ajouter un joueur, entrez son nom dans la boîte de saisie ci-dessous et appuyez sur Entrée ou cliquez sur le bouton qui apparaît. Pour enlever un joueur, cliquez tout simplement sur son nom dans le menu déroulant ci-dessous."
 L["Add"] = "Ajouter"
 L["Remove"] = "Enlever"
 
@@ -85,26 +91,38 @@ L["Monitor settings"] = "Paramètres du moniteur"
 L["Show monitor"] = "Afficher le moniteur"
 L["Lock monitor"] = "Verrouiller le moniteur"
 L["Show or hide the cooldown bar display in the game world."] = "Affiche ou non l'affichage des temps de recharge via des barres."
-L["Note that locking the cooldown monitor will hide the title and the drag handle and make it impossible to move it, resize it or open the display options for the bars."] = "Notez que le verrouillage du moniteur des temps de regarde cachera le titre et la poignée de saisie, rendant ainsi le moniteur impossible à déplacer ou à redimensionner. Il ne sera également pas possible d'ouvrir le menu des options des barres."
+L["Note that locking the cooldown monitor will hide the title and the drag handle and make it impossible to move it, resize it or open the display options for the bars."] = "Notez que le verrouillage du moniteur des temps de regarde masquera le titre et la poignée de saisie, rendant ainsi le moniteur impossible à déplacer ou à redimensionner. Il ne sera également pas possible d'ouvrir le menu des options des barres."
 L["Only show my own spells"] = "Afficher uniquement mes propres sorts"
 L["Toggle whether the cooldown display should only show the cooldown for spells cast by you, basically functioning as a normal cooldown display addon."] = "Affiche ou non uniquement les temps de recharge concernant votre personnage."
 L["Cooldown settings"] = "Paramètres des temps de recharge"
 L["Select which cooldowns to display using the dropdown and checkboxes below. Each class has a small set of spells available that you can view using the bar display. Select a class from the dropdown and then configure the spells for that class according to your own needs."] = "Choississez les temps de recharge à afficher en vous aidant du menu déroulant et des cases à cocher ci-dessous. Chaque classe possède un nombre limité de sorts que vous pouvez voir via l'affichage par barres. Choissisez une classe via le menu déroulant et configurer ses sorts selon vos besoins."
 L["Select class"] = "Choix de la classe"
+L["Never show my own spells"] = "Ne jamais afficher mes propres sorts"
+L["Toggle whether the cooldown display should never show your own cooldowns. For example if you use another cooldown display addon for your own cooldowns."] = "Enlève ou non vos propres temps de recharge de l'affichage des temps de recharge. À cocher par exemple si vous utilisez un autre addon pour afficher vos temps de recharge."
 -- monitor
+L["Right-Click me for options!"] = "Clic droit pour les options !"
 L["Bar Settings"] = "Param. des barres"
 L["Spawn test bar"] = "Afficher une barre de test"
 L["Use class color"] = "Couleur de classe"
-L["Bar height"] = "Hauteur des barres"
+L["Height"] = "Hauteur"
+L["Scale"] = "Échelle"
+L["Texture"] = "Texture"
 L["Icon"] = "Icône"
 L["Show"] = "Afficher"
 L["Duration"] = "Durée"
 L["Unit name"] = "Nom unité"
 L["Spell name"] = "Nom du sort"
 L["Short Spell name"] = "Nom des sorts raccourcis"
+L["Label Align"] = "Alignement du libellé"
+L["Left"] = "Gauche"
+L["Right"] = "Droite"
+L["Center"] = "Centre"
 
 -- Zone module
 L["Zone"] = "Zone"
 
 -- Version module
 L["Version"] = "Version"
+
+-- Loot module
+ L["Leave empty to make yourself Master Looter."] = "Laissez vide pour faire de vous le maître du butin."
