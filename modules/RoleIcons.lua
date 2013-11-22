7847f84246db84ca739804d10c187944a8899dd5
Benjamin Staneck <staneck@gmail.com>
2013-11-22 17:44:45 +0000

    add a file for nebula169 to paste stuff into.

---
+++ modules/RoleIcons.lua
@@ -0,0 +1,6 @@
+local oRA = LibStub("AceAddon-3.0"):GetAddon("oRA3")
+local util = oRA.util
+local module = oRA:NewModule("RoleIcons")
+local L = LibStub("AceLocale-3.0"):GetLocale("oRA3")
+
+module.VERSION = tonumber(("$Revision: 645 $"):sub(12, -3))
