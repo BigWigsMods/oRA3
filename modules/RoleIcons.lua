710b7712f4854a25cc90ab20ef066b48d898e94c
Kyle Buller <bullerk@gmail.com>
2013-12-02 04:48:03 +0000

    RoleIcons: hide totals when not in a raid, class color names in tooltip

--- modules/RoleIcons.lua
+++ modules/RoleIcons.lua
@@ -3,34 +3,40 @@ local module = oRA:NewModule("RoleIcons")
 
 module.VERSION = tonumber(("$Revision$"):sub(12, -3))
 
-local countIcons
-local raidIcons = setmetatable({}, { __index = function(t,i)
-	local parent = _G["RaidGroupButton"..i]
-	local icon = CreateFrame("Frame", nil, parent)
-	icon:SetSize(14, 14)
-	icon:SetPoint("RIGHT", parent.subframes.level, "LEFT", 2, 0)
-	RaiseFrameLevel(icon)
-
-	local texture = icon:CreateTexture(nil, "ARTWORK")
-	texture:SetAllPoints()
-	texture:SetTexture("Interface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES")
-	icon.texture = texture
-
-	icon:Hide()
-
-	t[i] = icon
-	return icon
-end })
+local countIcons -- frame containing the totals by role
 
 local updateIcons
 do
+	local roleIcons = setmetatable({}, { __index = function(t,i)
+		local parent = _G["RaidGroupButton"..i]
+		local icon = CreateFrame("Frame", nil, parent)
+		icon:SetSize(14, 14)
+		icon:SetPoint("RIGHT", parent.subframes.level, "LEFT", 2, 0)
+		RaiseFrameLevel(icon)
+
+		local texture = icon:CreateTexture(nil, "ARTWORK")
+		texture:SetAllPoints()
+		texture:SetTexture("Interface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES")
+		icon.texture = texture
+
+		icon:Hide()
+
+		t[i] = icon
+		return icon
+	end })
+
 	local count = {}
 	function updateIcons()
+		if not IsInRaid() then
+			countIcons:Hide()
+			return
+		end
+
 		wipe(count)
 		for i = 1, GetNumGroupMembers() do
 			local button = _G["RaidGroupButton"..i]
 			if button and button.subframes then -- make sure the raid button is set up
-				local icon = raidIcons[i]
+				local icon = roleIcons[i]
 				local role = UnitGroupRolesAssigned("raid"..i)
 				if role and role ~= "NONE" then
 					icon.texture:SetTexCoord(GetTexCoordsForRoleSmallCircle(role))
@@ -44,6 +50,7 @@ do
 		for role, icon in next, countIcons.icons do
 			icon.count:SetText(count[role] or 0)
 		end
+		countIcons:Show()
 	end
 end
 
@@ -51,18 +58,21 @@ local createCountIcons
 do
 	local roster = {}
 	for i=1,NUM_RAID_GROUPS do roster[i] = {} end
+	local function sortColoredNames(a, b) return a:sub(11) < b:sub(11) end
 	local function onEnter(self)
 		local role = self.role
 		GameTooltip:SetOwner(self, "ANCHOR_BOTTOMLEFT")
 		GameTooltip:SetText(_G["INLINE_" .. role .. "_ICON"] .. _G[role])
 		for i = 1, GetNumGroupMembers() do
-			local name, _, group, _, _, _, _, _, _, _, _, groupRole = GetRaidRosterInfo(i)
+			local name, _, group, _, _, class, _, _, _, _, _, groupRole = GetRaidRosterInfo(i)
 			if groupRole == role then
-				tinsert(roster[group], name)
+				local color = oRA.classColors[class]
+				local coloredName = ("|cff%02x%02x%02x%s"):format(color.r * 255, color.g * 255, color.b * 255, name:gsub("%-.+", "*"))
+				tinsert(roster[group], coloredName)
 			end
 		end
 		for group, list in ipairs(roster) do
-			sort(list)
+			sort(list, sortColoredNames)
 			for _, name in ipairs(list) do
 				GameTooltip:AddLine(("[%d] %s"):format(group, name), 1, 1, 1)
 			end
