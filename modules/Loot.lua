47199e1728acbce0a504f67296a34cf9e3747866
funkydude <funkydude@users.noreply.github.com>
2013-12-23 01:14:38 +0000

    Loot: Delay loot setting, hopefully fixes #154.

--- modules/Loot.lua
+++ modules/Loot.lua
@@ -121,7 +121,7 @@ function module:OnRegister()
 	db = self.db.profile
 
 	oRA.RegisterCallback(self, "OnPromoted", "SetLoot")
-	oRA.RegisterCallback(self, "OnGroupChanged")
+	oRA.RegisterCallback(self, "OnGroupChanged", "SetLoot")
 	oRA.RegisterCallback(self, "OnProfileUpdate", function()
 		db = self.db.profile
 	end)
@@ -129,35 +129,37 @@ function module:OnRegister()
 	oRA:RegisterModuleOptions("Loot", getOptions, LOOT_METHOD)
 end
 
-function module:OnGroupChanged(_, groupStatus)
-	if groupStatus > 0 then
-		self:SetLoot()
+do
+	local function updateLoot(self)
+		self.timer = nil
+		if UnitIsGroupLeader("player") then
+			local method, threshold, master
+			if IsInRaid() then
+				method = db.raid.method
+				threshold = db.raid.threshold
+				master = db.raid.master
+			else
+				method = db.party.method
+				threshold = db.party.threshold
+				master = db.party.master
+			end
+			if not master or master == "" then master = UnitName("player") end
+			local current = GetLootMethod()
+			if current and current == method then return end
+			SetLootMethod(method, master, threshold)
+			if method == "master" or method == "group" then
+				self:ScheduleTimer(SetLootThreshold, 2, threshold)
+			end
+			-- SetLootMethod("method"[,"masterPlayer" or ,threshold])
+			-- method  "group", "freeforall", "master", "neeedbeforegreed", "roundrobin".
+			-- threshold  0 poor  1 common  2 uncommon  3 rare  4 epic  5 legendary  6 artifact
+		end
+	end
+
+	function module:SetLoot()
+		if db.enable and not self.timer and IsInGroup() then
+			-- Delay loot setting, hopefully fixes #154.
+			self.timer = self:ScheduleTimer(updateLoot, 2, self)
+		end
 	end
 end
-
-function module:SetLoot()
-	if not db.enable then return end
-	if UnitIsGroupLeader("player") then
-		local method, threshold, master
-		if IsInRaid() then
-			method = db.raid.method
-			threshold = db.raid.threshold
-			master = db.raid.master
-		else
-			method = db.party.method
-			threshold = db.party.threshold
-			master = db.party.master
-		end
-		if not master or master == "" then master = UnitName("player") end
-		local current = GetLootMethod()
-		if current and current == method then return end
-		SetLootMethod(method, master, threshold)
-		if method == "master" or method == "group" then
-			self:ScheduleTimer(SetLootThreshold, 3, threshold)
-		end
-		-- SetLootMethod("method"[,"masterPlayer" or ,threshold])
-		-- method  "group", "freeforall", "master", "neeedbeforegreed", "roundrobin".
-		-- threshold  0 poor  1 common  2 uncommon  3 rare  4 epic  5 legendary  6 artifact
-	end
-end
-
