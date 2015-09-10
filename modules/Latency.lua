59141195b7cddf35b6589fc7de083b1f3adea58b
funkydude <funkydude@users.noreply.github.com>
2015-09-10 13:19:27 +0000

    Swap our latency module over to using LibLatency so that it's cross compatible with other addons and doesn't require oRA be installed to use it. Alternatively the user can also install the library as a standalone addon.

--- modules/Latency.lua
+++ modules/Latency.lua
@@ -1,12 +1,13 @@
 
 -- Latency is requested/transmitted when opening the list.
--- Latency information will be available from the oRA3 gui for everyone.
+-- This module is a display wrapper for LibLatency.
 
 local addonName, scope = ...
 local oRA = scope.addon
 local util = oRA.util
 local module = oRA:NewModule("Latency")
 local L = scope.locale
+local LL = LibStub("LibLatency")
 
 local latency = {}
 
@@ -20,7 +21,6 @@ function module:OnRegister()
 	)
 	oRA.RegisterCallback(self, "OnShutdown")
 	oRA.RegisterCallback(self, "OnListSelected")
-	oRA.RegisterCallback(self, "OnCommReceived")
 	oRA.RegisterCallback(self, "OnGroupChanged")
 
 	SLASH_ORALATENCY1 = "/ralag"
@@ -38,40 +38,25 @@ function module:OnShutdown()
 	wipe(latency)
 end
 
-do
-	local prev = 0
-	function module:OnListSelected(event, list)
-		if list == L.latency then
-			local t = GetTime()
-			if t-prev > 7 then
-				prev = t
-				self:SendComm("QueryLag")
-			end
-		end
+function module:OnListSelected(event, list)
+	if list == L.latency then
+		LL:RequestLatency()
 	end
 end
 
-do
-	local prev = 0
-	function module:OnCommReceived(_, sender, prefix, latencyHome, latencyWorld)
-		if prefix == "QueryLag" then
-			local t = GetTime()
-			if t-prev > 7 then
-				prev = t
-				local _, _, latencyHome, latencyWorld = GetNetStats() -- average world latency
-				self:SendComm("Lag", latencyHome, latencyWorld)
-			end
-		elseif prefix == "Lag" then
-			local k = util.inTable(latency, sender, 1)
-			if not k then
-				k = #latency + 1
-				latency[k] = { sender }
-			end
-			latency[k][2] = tonumber(latencyHome)
-			latency[k][3] = tonumber(latencyWorld)
+local function update(latencyHome, latencyWorld, player, channel)
+	if channel == "GUILD" then return end
 
-			oRA:UpdateList(L.latency)
-		end
+	local k = util.inTable(latency, player, 1)
+	if not k then
+		k = #latency + 1
+		latency[k] = { player }
 	end
+	latency[k][2] = latencyHome
+	latency[k][3] = latencyWorld
+
+	oRA:UpdateList(L.latency)
 end
+
+LL:Register(module, update)
 
