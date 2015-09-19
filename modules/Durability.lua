7c9a3f9849fce60277c80340094574bd4a6a7976
funkydude <funkydude@users.noreply.github.com>
2015-09-19 14:05:06 +0000

    Swap our durability module over to using LibDurability so that it's cross compatible with other addons and doesn't require oRA be installed to use it. Alternatively the user can also install the library as a standalone addon.

--- modules/Durability.lua
+++ modules/Durability.lua
@@ -1,12 +1,13 @@
 
--- Durability is transmitted when the player dies or zones or closes a merchant window
--- Durability information will be available from the oRA3 gui for everyone.
+-- Durability is requested/transmitted when opening the list.
+-- This module is a display wrapper for LibDurability.
 
 local addonName, scope = ...
 local oRA = scope.addon
 local util = oRA.util
 local module = oRA:NewModule("Durability")
 local L = scope.locale
+local LD = LibStub("LibDurability")
 
 local durability = {}
 
@@ -16,14 +17,11 @@ function module:OnRegister()
 		L.durability,
 		durability,
 		L.name,
-		L.average,
-		L.minimum,
+		L.durability,
 		L.broken
 	)
-	oRA.RegisterCallback(self, "OnStartup")
 	oRA.RegisterCallback(self, "OnShutdown")
 	oRA.RegisterCallback(self, "OnListSelected")
-	oRA.RegisterCallback(self, "OnCommReceived")
 	oRA.RegisterCallback(self, "OnGroupChanged")
 
 	SLASH_ORADURABILITY1 = "/radur"
@@ -37,54 +35,28 @@ function module:OnGroupChanged()
 	oRA:UpdateList(L.durability)
 end
 
-function module:OnStartup()
-	self:RegisterEvent("PLAYER_DEAD", "CheckDurability")
-	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "CheckDurability")
-	self:RegisterEvent("MERCHANT_CLOSED", "CheckDurability")
-
-	self:CheckDurability()
-end
-
 function module:OnShutdown()
 	wipe(durability)
-	self:UnregisterAllEvents()
 end
 
 function module:OnListSelected(event, list)
 	if list == L.durability then
-		self:CheckDurability()
+		LD:RequestDurability()
 	end
 end
 
-function module:CheckDurability()
-	local cur, max, broken, vmin = 0, 0, 0, 100
-	for i = 1, 18 do
-		local imin, imax = GetInventoryItemDurability(i)
-		if imin and imax then
-			vmin = math.min(math.floor(imin / imax * 100), vmin)
-			if imin == 0 then broken = broken + 1 end
-			cur = cur + imin
-			max = max + imax
-		end
-	end
-	local perc = math.floor(cur / max * 100)
-	self:SendComm("Durability", perc, vmin, broken)
-end
-
-function module:OnCommReceived(_, sender, prefix, perc, minimum, broken)
-	if prefix == "RequestUpdate" then
-		self:CheckDurability()
-	elseif prefix == "Durability" then
-		local k = util.inTable(durability, sender, 1)
+do
+	local function update(percent, broken, player)
+		local k = util.inTable(durability, player, 1)
 		if not k then
-			durability[#durability + 1] = { sender }
+			durability[#durability + 1] = { player }
 			k = #durability
 		end
-		durability[k][2] = tonumber(perc)
-		durability[k][3] = tonumber(minimum)
-		durability[k][4] = tonumber(broken)
+		durability[k][2] = tonumber(percent)
+		durability[k][3] = tonumber(broken)
 
 		oRA:UpdateList(L.durability)
 	end
+	LD:Register(module, update)
 end
 
