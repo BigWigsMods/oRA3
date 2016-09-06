b38059101fb85aa094d91e240be8ce0092794747
Kyle Buller <bullerk@gmail.com>
2016-09-05 19:32:41 -0500

    Alerts: Always enable player/spell links and remove brackets from names

--- modules/Alerts.lua
+++ modules/Alerts.lua
@@ -378,10 +378,10 @@ do -- COMBAT_LOG_EVENT_UNFILTERED
 	local function getName(name, guid, flags)
 		local petOwner = petOwnerMap[guid]
 		if petOwner then
-			petOwner = module.db.profile.playerLink and ("|Hplayer:%s|h[%s]|h"):format(petOwner, petOwner:gsub("%-.*", "")) or petOwner:gsub("%-.*", "")
+			petOwner = ("|Hplayer:%s|h%s|h"):format(petOwner, petOwner:gsub("%-.*", ""))
 			return L["%s's %s"]:format(petOwner, name or UNKNOWN)
 		elseif name and bit_band(flags, FILTER_FRIENDLY_PLAYERS) == FILTER_FRIENDLY_PLAYERS then
-			return module.db.profile.playerLink and ("|Hplayer:%s|h[%s]|h"):format(name, name:gsub("%-.*", "")) or name:gsub("%-.*", "")
+			return ("|Hplayer:%s|h%s|h"):format(name, name:gsub("%-.*", ""))
 		end
 		return name or UNKNOWN
 	end
@@ -483,10 +483,10 @@ do -- COMBAT_LOG_EVENT_UNFILTERED
 			end
 			local srcOutput = ("%s|c%s%s|r"):format(getIconString(srcRaidFlags), srcColor, getName(srcName, srcGUID, srcFlags))
 			local dstOutput = ("%s|c%s%s|r"):format(getIconString(dstRaidFlags), dstColor, getName(dstName, dstGUID, dstFlags))
-			local spellOutput = module.db.profile.spellLink and GetSpellLink(spellId) or ("|cff71d5ff%s|r"):format(spellName)
+			local spellOutput = GetSpellLink(spellId)
 			local extraSpellOuput
 			if tonumber(extraSpellId) then -- kind of hacky, pretty print the extra spell for interrupts/breaks/dispels
-				extraSpellOuput = module.db.profile.spellLink and GetSpellLink(extraSpellId) or ("|cff71d5ff%s|r"):format(extraSpellName)
+				extraSpellOuput = GetSpellLink(extraSpellId)
 			elseif (extraSpellId ~= "BUFF" and extraSpellId ~= "DEBUFF") then
 				extraSpellOuput = extraSpellId
 			end
@@ -498,15 +498,17 @@ do -- COMBAT_LOG_EVENT_UNFILTERED
 	end)
 
 	-- Codex handling
-	function module:UNIT_SPELLCAST_SUCCEEDED(unit, spellName, _, _, spellId)
-		if spellId == 227564 then -- XXX 226234 for Tranquil Mind (100+)
+	local prev = nil
+	function module:UNIT_SPELLCAST_SUCCEEDED(unit, spellName, _, spellCastGUID, spellId)
+		if spellId == 226234 and spellCastGUID ~= prev then -- Codex of the Tranquil Mind
+			prev = spellCastGUID
 			local srcName, srcGUID, srcRaidFlags = self:UnitName(unit), UnitGUID(unit), 0
 			local icon = GetRaidTargetIndex(unit)
 			if icon then
 				srcRaidFlags = _G["COMBATLOG_OBJECT_RAIDTARGET" .. icon]
 			end
-			local srcOutput = ("%s|cff40ff40%s|r"):format(getIconString(srcRaidFlags), getName(srcName, srcGUID))
-			local spellOutput = module.db.profile.spellLink and GetSpellLink(spellId) or ("|cff71d5ff%s|r"):format(spellName)
+			local srcOutput = ("%s|cff40ff40%s|r"):format(getIconString(srcRaidFlags), getName(srcName, srcGUID, 0))
+			local spellOutput = GetSpellLink(spellId)
 			self:Codex(srcOutput, nil, spellOutput)
 		end
 	end
@@ -634,7 +636,7 @@ do
 	local spiritOfRedemption = GetSpellInfo(27827)
 	local feignDeath = GetSpellInfo(5384)
 
-	local soulstoneLink, soulstone = (GetSpellLink(20707)), ("|cff71d5ff%s|r"):format((GetSpellInfo(20707)))
+	local soulstone = GetSpellLink(20707)
 
 	local total = 0
 	local function checkDead(self, elapsed)
@@ -652,10 +654,9 @@ do
 					soulstoneList[name] = nil
 				elseif not UnitIsDead(name) and UnitIsConnected(name) and not UnitIsFeignDeath(name) and not UnitBuff(name, feignDeath) and not UnitBuff(name, spiritOfRedemption) then
 					soulstoneList[name] = nil
-					name = module.db.profile.playerLink and ("|Hplayer:%s|h[%s]|h"):format(name, name:gsub("%-.*", "")) or name:gsub("%-.*", "")
+					name = ("|Hplayer:%s|h%s|h"):format(name, name:gsub("%-.*", ""))
 					local srcOutput = ("|cff40ff40%s|r"):format(name)
-					local spellOutput = module.db.profile.spellLink and soulstoneLink or soulstone
-					module:Spam("combatRes", L["%s used %s"]:format(srcOutput, spellOutput))
+					module:Spam("combatRes", L["%s used %s"]:format(srcOutput, soulstone))
 				end
 			end
 
@@ -704,8 +705,6 @@ function module:OnRegister()
 			enableForRaid = true,
 			enableForLFG = false,
 
-			spellLink = true,
-			playerLink = true,
 			classColor = true,
 			icons = true,
 			groupOnly = true,
@@ -961,18 +960,6 @@ function GetOptions()
 						},
 					},
 
-					spellLink = {
-						name = L["Use Spell Links"],
-						desc = L["Display spell names as clickable spell links."],
-						type = "toggle",
-						order = 39,
-					},
-					playerLink = {
-						name = L["Use Player Links"],
-						desc = L["Display player names as clickable player links."],
-						type = "toggle",
-						order = 40,
-					},
 					classColor = {
 						name = L["Use Class Colors"],
 						desc = L["Use class colored names for people in your group instead of using source/target colors."],
