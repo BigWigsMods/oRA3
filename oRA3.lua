f8f30fb4c569f666da2fd130df2dcbce132e339f
Kyle Buller <bullerk@gmail.com>
2021-01-13 23:29:05 -0600

    Revert back to using :IsPromoted when disbanding

--- oRA3.lua
+++ oRA3.lua
@@ -112,7 +112,7 @@ local scrollhighs = {} -- scroll highlights
 local secureScrollhighs = {} -- clickable secure scroll highlights
 
 local function actuallyDisband()
-	if groupStatus > 0 and groupStatus < 3 and (playerPromoted or 0) > 1 then
+	if groupStatus > 0 and groupStatus < 3 and (addon:IsPromoted() or 0) > 1 then
 		SendChatMessage("<oRA3> ".. L.disbandingGroupChatMsg, IsInRaid() and "RAID" or "PARTY")
 		C_Timer.After(3, C_PartyInfo.LeaveParty)
 		for _, unit in next, groupMembers do
